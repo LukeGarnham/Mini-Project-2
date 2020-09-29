87f41b37fd0acc24d5c38d5371d858980e1910a7
LukeGarnham <lukegarnham@msn.com>
2020-09-29 14:51:31 +0000

    Fixed bugs for the gh-user-data & gh-repo-data elements.

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -49,6 +49,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $("gh-user-data").html("");
+    $("gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -82,3 +84,5 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
